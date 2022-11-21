8d9132e4cc78ac3336d512f9807eb8620e86669b
GraysTania <101881748+GraysTania@users.noreply.github.com>
2022-11-21 11:55:16 +0200

    2222

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+console.log(SimpleLightbox);
+
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+
+const markup = galleryItems.reduce(
+  (acc, { preview, original, description }) =>
+    acc +
+    `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`,
+  ''
+);
+
+refs.gallery.insertAdjacentHTML('beforeend', markup);
+
+new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  formEmail: document.querySelector('.feedback-form input'),
+  formMessage: document.querySelector('.feedback-form textarea'),
+};
+
+refs.form.addEventListener('input', throttle(handleInputForm, 500));
+refs.form.addEventListener('submit', handleSubmitForm);
+
+let objForm = {
+  email: '',
+  message: '',
+};
+
+function handleInputForm(e) {
+  objForm.email = refs.formEmail.value;
+  objForm.message = refs.formMessage.value;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(objForm));
+}
+
+const formSave = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+if (formSave) {
+  refs.formEmail.value = formSave.email;
+  refs.formMessage.value = formSave.message;
+}
+
+function handleSubmitForm(e) {
+  e.preventDefault();
+  refs.form.reset();
+  localStorage.removeItem('feedback-form-state');
+  console.log(formSave);
+}
